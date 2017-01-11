b6f824f9d20b358b370023b23b4648fe1259bfc7
Chance Smith <chancesmithb@gmail.com>
2017-01-10 21:00:22 -0600

    adds new-members gragh, adds total-members graph

---
+++ jobs/new-members.rb
@@ -0,0 +1,11 @@
+SCHEDULER.every '10s' do
+	points = [{:x=>1, :y=>10},
+						{:x=>2, :y=>5},
+						{:x=>3, :y=>12},
+						{:x=>4, :y=>8},
+						{:x=>5, :y=>1},
+						{:x=>6, :y=>4}
+					]
+
+  send_event('new-members', points: points)
+end
---
+++ jobs/total-members.rb
@@ -0,0 +1,11 @@
+SCHEDULER.every '10s' do
+  points = [{:x=>1, :y=>107},
+  					{:x=>2, :y=>104},
+  					{:x=>3, :y=>114},
+  					{:x=>4, :y=>111},
+  					{:x=>5, :y=>108},
+  					{:x=>6, :y=>112}
+  				]
+
+  send_event('total-members', points: points)
+end
