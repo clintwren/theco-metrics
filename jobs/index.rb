59b3f4602712db09317c6372b3f9a98d4001b966
Chance Smith <chancesmithb@gmail.com>
2017-01-23 18:39:57 -0600

    updates metircs and adds tours metric - bye coffee

--- jobs/index.rb
+++ jobs/index.rb
@@ -1,7 +1,7 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m' do
-  send_event('members', {value:108})
-  send_event('visitors', {value:200})
-  send_event('coffee', {value:13.5})
+  # send_event('members', {value:108})
+  # send_event('visitors', {value:200})
+  send_event('tours', {value:15})
   send_event('mentoring', {value:18})
 end
